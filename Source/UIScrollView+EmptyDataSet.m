99e81847b2e0a1848c80e182b143d145a590e7dc
Thanh Cao <ctt.bk.hcmut2009@gmail.com>
2016-08-31 23:37:12 +0700

    Fix bug #263 - Button tap on custom view

--- Source/UIScrollView+EmptyDataSet.m
+++ Source/UIScrollView+EmptyDataSet.m
@@ -1023,19 +1023,26 @@ Class dzn_baseClassToSwizzleForTarget(id target)
 
 - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
 {
-    UIView *hitView = [super hitTest:point withEvent:event];
-    
-    // Return any UIControl instance such as buttons, segmented controls, switches, etc.
-    if ([hitView isKindOfClass:[UIControl class]]) {
+    if(_customView != nil){
+        CGRect newFrame = _customView.frame;
+        newFrame.size.height = self.frame.size.height;
+        _customView.frame = newFrame;
+        UIView *hitView = [_customView hitTest:point withEvent:event];
         return hitView;
+    }else{
+        UIView *hitView = [super hitTest:point withEvent:event];
+        
+        // Return any UIControl instance such as buttons, segmented controls, switches, etc.
+        if ([hitView isKindOfClass:[UIControl class]]) {
+            return hitView;
+        }
+        
+        // Return either the contentView or customView
+        if ([hitView isEqual:_contentView] || [hitView isEqual:_customView]) {
+            return hitView;
+        }
+        return nil;
     }
-    
-    // Return either the contentView or customView
-    if ([hitView isEqual:_contentView] || [hitView isEqual:_customView]) {
-        return hitView;
-    }
-    
-    return nil;
 }
 
 @end
